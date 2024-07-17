ba67e426968f5809d72b091e0c3429f54e6e95d3
MH2425 <moongazing@gmail.com>
2024-07-17 11:42:55 +0700

    Another simple recursion method

---
+++ src/Recursion/palindromic_phrase/PalindromicPhrase.java
@@ -0,0 +1,39 @@
+package Recursion.palindromic_phrase;
+
+import java.util.*;
+public class PalindromicPhrase {
+    public static boolean isPalindrome(String text) {
+        int length = text.length();
+        if (length <= 1) {
+            return true;
+        } else {
+            char first = Character.toLowerCase(text.charAt(0));
+            char last = Character.toLowerCase(text.charAt(length - 1));
+
+            if (Character.isLetter(first) && Character.isLetter(last)) {
+                if (first == last) {
+                    String shorterText = text.substring(1, length - 1);
+                    return isPalindrome(shorterText);
+                } else {
+                    return false;
+                }
+            } else if (!Character.isLetter(last)) {
+                String shorterText = text.substring(0, length - 1);
+                return isPalindrome(shorterText);
+            } else {
+                String shorterText = text.substring(1);
+                return isPalindrome(shorterText);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a phrase: ");
+        String text = sc.nextLine();
+        if (isPalindrome(text)) {
+            System.out.println("The phrase " + text + " is palindromic");
+        } else {
+            System.out.println("The phrase " + text + " is not a palindromic");
+        }
+    }
+}
