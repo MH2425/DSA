82579615f16cdbbbea01a22db5d90f92a6631a5f
MH2425 <moongazing@gmail.com>
2024-08-05 10:07:36 +0700

    fix logical error

--- src/Recursion/mutual_recursive/ExpressionCalculator.java
+++ src/Recursion/mutual_recursive/ExpressionCalculator.java
@@ -10,7 +10,7 @@ public class ExpressionCalculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter an expression: ");
-        String input = in.nextLine();
+        String input = in.nextLine().replace(" ", "");
         Evaluator e = new Evaluator(input);
         int value = e.getExpressionValue();
         System.out.println(input + " = " + value);
