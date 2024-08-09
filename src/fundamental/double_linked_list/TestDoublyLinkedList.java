5d2c95e74b7073d868488cb1dac9eefc38287b87
MH2425 <moongazing@gmail.com>
2024-08-09 15:31:19 +0700

    recreate doublyLinkedList data structure

---
+++ src/fundamental/double_linked_list/TestDoublyLinkedList.java
@@ -0,0 +1,10 @@
+package fundamental.double_linked_list;
+
+public class TestDoublyLinkedList {
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        doublyLinkedList.addFirst(1);
+        doublyLinkedList.addLast(3);
+        System.out.println(doublyLinkedList);
+    }
+}
